3f3bbc8feb63e2e3a5e5359c97f909d5b3f60779
claudiu <klau2005@x200m-debian.acasa>
2016-10-02 14:41:17 +0300

    added calc app in numFunctions (ex 5-6)

---
+++ numFunctions.py
@@ -0,0 +1,88 @@
+def product(x, y):
+    return x * y
+
+def get_number(start, end):
+    err_msg = "Please enter only numbers between %d and %d!" % (start, end)
+    while True:
+        try:
+            number = int(input("Enter number between %d and %d: " % (start, end)))
+        except ValueError:
+            print(err_msg)
+            continue
+        if  number < start or number > end:
+            print(err_msg)
+            continue
+        else:           
+            break
+    return number
+
+def score2grade():
+    print("Enter score below to return grade")
+    score = get_number(1, 100)
+    if score >= 90:
+        grade = "A"
+    elif score >= 80:
+        grade = "B"
+    elif score >= 70:
+        grade = "C"
+    elif score >= 60:
+        grade = "D"
+    else:
+        grade = "F"
+    return grade
+
+def is_leap(year):
+    if (year % 4 == 0) and ((year % 100 != 0) or (year % 100 == 0 and year % 400 == 0)):
+        return year
+    else:
+        False
+
+def calculate_change():
+    err_msg = "Enter ammount below to calculate coins"
+    coins = {1: "penny", 5: "nickel", 10: "dime", 25: "quarter"}
+    amount = get_number(1, 99)
+    change_dict = {}
+    for value in sorted(coins, reverse=True):
+        change_dict[coins[value]] = divmod(amount, value)[0]
+        amount = divmod(amount, value)[1]
+    return change_dict
+
+def type_err():
+    print(
+
+def calc_app():
+    oper_err = "Use only '+', '-', '*', '/', '%' or '**' for OP"
+    type_err = "Use only integers/floats for N1 and N2"
+    while True:
+        calc = input("Enter 2 numbers and an operand (N1 OP N2): ")
+        try:
+            n1 = float(calc.split()[0])
+            n2 = float(calc.split()[2])
+        except ValueError:
+            print(type_err)
+            continue
+        op = str(calc.split()[1])
+        if op == "+":
+            result = n1 + n2
+            break
+        elif op == "-":
+            result = n1 - n2
+            break
+        elif op == "*":
+            result = n1 * n2
+            break
+        elif op == "/":
+            result = n1 / n2
+            break
+        elif op == "%":
+            result = n1 % n2
+            break
+        elif op == "**":
+            result = n1 ** n2
+            break
+        else:
+            print(oper_err)
+            continue
+    if str(result).split(".")[1] == "0":
+        result = int(result)
+    return result
